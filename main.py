b599e7059e54da5700842dd5fa0fc5fcd4dc26f4
Sayan Das <poke19962008@gmail.com>
2015-12-10 02:18:24 +0530

    completed api for institute

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import requests
 root = "https://www.codechef.com/api/rankings/"
 
 def onlyInstitute(param, instiName):
+	notif = []
+
 	url = root + param + instiName
 	req = requests.get(url)
 	print req.status_code
@@ -12,7 +14,6 @@ def onlyInstitute(param, instiName):
 		raise Exception('Cannot connect Code Chef\'s server')
 	else:
 		data = req.json()['list']
-		# print json.dumps(data['list'])
 
 		if not os.path.isfile('instiLog.json'):
 			file = open('instiLog.json', 'w')
@@ -21,7 +22,23 @@ def onlyInstitute(param, instiName):
 			file = open('instiLog.json', 'r')
 			prevData = json.loads(file.read())
 
-		
+			for user in data:
+				found = False
+				prevUser = {}
+				for iterateUser in prevData:
+					if iterateUser['user_handle'] == user['user_handle']:
+						found = True
+						prevUser = iterateUser
+
+				if not found:
+					print "Cannot find "+ user['user_handle']
+				else:
+					if user['score'] != prevUser['score']:
+						for prob in user['problems_status']:
+							if not prevUser['problems_status'].has_key(prob):
+								notif.append({ 'user_handle': user['user_handle'], 'problem': prob, 'score': user['problems_status'][prob]['score'] })
+	print notif
+
 
 
 parser = argparse.ArgumentParser()
@@ -47,6 +64,6 @@ try:
 			instiName = re.sub(' ', '%20', args.instiName)
 			param = "?filterBy=Institution%3D"
 			onlyInstitute(param, instiName)
-			time.sleep(1)
+			time.sleep(3)
 except ValueError as e:
 	print e
